98b0062bfdf7038fa2afbfbe2b988b9b944688b7
Ario <ariowistus@gmail.com>
2023-02-05 18:45:09 +0100

    commit1

---
+++ src/Purchase.java
@@ -0,0 +1,37 @@
+import java.time.LocalDate;
+
+public class Purchase {
+    private final Client buyer;
+    private final Product product;
+    private final long quantity;
+    private final Delivery delivery;
+    private final Payment payment;
+    private final LocalDate when;
+    private Status status = Status.PAID;
+
+    public Purchase(Client buyer, Product product, long quantity, Delivery delivery, Payment payment, LocalDate when) {
+        this.buyer = buyer;
+        this.product = product;
+        this.quantity = quantity;
+        this.delivery = delivery;
+        this.payment = payment;
+        this.when = when;
+    }
+
+    // konstruktory, gettery itp
+    public enum Delivery {
+        IN_POST,
+        UPS,
+        DHL
+    }
+    public enum Payment {
+        CASH,
+        BLIK,
+        CREDIT_CARD
+    }
+    public enum Status {
+        PAID,
+        SENT,
+        DONE
+    }
+}
